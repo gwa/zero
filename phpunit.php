d0c6d4bd7d81e63edcb5a8a8d0b19fb1656add95
anolilab <info@anolilab.de>
2015-06-05 14:31:13 +0200

    zero 2.0

--- phpunit.php
+++ phpunit.php
@@ -3,19 +3,3 @@
 if (file_exists($composer_autoload = __DIR__ . '/../vendor/autoload.php')) {
     require_once($composer_autoload);
 }
-
-$testsDir = getenv('WP_TESTS_DIR');
-
-if (!$testsDir) {
-    $testsDir = '/tmp/wordpress-tests-lib';
-}
-
-require_once $testsDir . '/includes/functions.php';
-
-function manuallyLoadPlugin()
-{
-    require dirname(__FILE__) . '/../wp-content/plugins/timber/timber.php';
-}
-
-tests_add_filter('muplugins_loaded', 'manuallyLoadPlugin');
-require $testsDir . '/includes/bootstrap.php';
